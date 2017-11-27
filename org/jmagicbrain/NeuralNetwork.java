968e91f9c7bbfab227fe4abe657d6ea51c4e14e2
Carlos Soto Pérez <carlos348@outlook.com>
2017-11-27 13:37:21 -0600

    Working with builder

--- org/jmagicbrain/NeuralNetwork.java
+++ org/jmagicbrain/NeuralNetwork.java
@@ -1,20 +1,70 @@
 package org.jmagicbrain;
 
+import org.jmagicbrain.exceptions.InvalidNeuralNetworkArguments;
+import org.jmagicbrain.functions.ActivationFunction;
 import org.jmagicbrain.functions.ErrorFunction;
+
+import java.util.LinkedList;
 
 public class NeuralNetwork {
 
-    private NeuralNetwork(){}
+    private double[][] layers;
+    private double[][][] weightsMatrix;
+    private double numberOfWeights;
 
-    public static class NeuralNetworkBuilder {
-        private static ErrorFunction errorFunction;
+    private NeuralNetwork(ErrorFunction errorFunction, ActivationFunction activationFunction, Object trainingMethod, Integer ... layers){
+        // TODO: Body
+    }
 
-        public static void setErrorFunction(ErrorFunction errorFunction) {
-            NeuralNetworkBuilder.errorFunction = errorFunction;
+    public double[][][] getWeights(){
+        return weightsMatrix;
+    }
+
+    public class NeuralNetworkBuilder {
+        private ErrorFunction errorFunction;
+        private ActivationFunction activationFunction;
+        private Object trainingMethod;
+        private LinkedList<Integer> neuronQueue;
+
+        public NeuralNetworkBuilder(){
+            neuronQueue = new LinkedList<Integer>();
         }
 
-        public NeuralNetwork build() {
-            return new NeuralNetwork();
+        public NeuralNetworkBuilder setErrorFunction(ErrorFunction errorFunction) {
+            this.errorFunction = errorFunction;
+            return this;
+        }
+
+        public NeuralNetworkBuilder setActivationFunction(ActivationFunction activationFunction){
+            this.activationFunction = activationFunction;
+            return this;
+        }
+
+        public NeuralNetworkBuilder setTrainingMethod(){
+            return this;
+        }
+
+        public NeuralNetworkBuilder addLayer(int numberOfNeurons){
+            this.neuronQueue.add(numberOfNeurons);
+            return this;
+        }
+
+        public NeuralNetwork build() throws InvalidNeuralNetworkArguments{
+            Integer[] layers = new Integer[neuronQueue.size()];
+            neuronQueue.toArray(layers);
+            if(layers.length == 0){
+                throw new InvalidNeuralNetworkArguments("There are no layers");
+            }
+            if(errorFunction == null){
+                throw new InvalidNeuralNetworkArguments("Error function not set");
+            }
+            if(activationFunction == null){
+                throw  new InvalidNeuralNetworkArguments("Activation function not set");
+            }
+            if(trainingMethod == null){
+                throw new InvalidNeuralNetworkArguments("Training method not set");
+            }
+            return new NeuralNetwork(errorFunction, activationFunction, trainingMethod, layers);
         }
     }
 }
--- org/jmagicbrain/exceptions/InvalidNeuralNetworkArguments.java
+++ org/jmagicbrain/exceptions/InvalidNeuralNetworkArguments.java
@@ -1,4 +1,15 @@
 package org.jmagicbrain.exceptions;
 
 public class InvalidNeuralNetworkArguments extends RuntimeException {
+
+    private static String NAME = "InvalidNeuralNetworkArguments";
+
+    public InvalidNeuralNetworkArguments(String message){
+        super(NAME + " " + message);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
