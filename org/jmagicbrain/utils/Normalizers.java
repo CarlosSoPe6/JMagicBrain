06470e6e343522575a5ab5c6d715a8d2e1db9e7d
Carlos Soto Pérez <carlos348@outlook.com>
2017-11-29 18:14:46 -0600

    Funciones de utilidad

--- org/jmagicbrain/utils/Normalizers.java
+++ org/jmagicbrain/utils/Normalizers.java
@@ -1,13 +1,23 @@
 package org.jmagicbrain.utils;
 
-import org.jetbrains.annotations.Contract;
-
 public class Normalizers {
-    @Contract(pure = true)
+
+    /**
+     * Entrega un valor de 0 a 1
+     * @param value Valor a normalizar
+     * @param min Valor mínimo posible
+     * @param max Valor máximo posible
+     * @return 'value' normalizado de 0 a 1
+     */
     public static double normalizeRange(double value, double min, double max){
         return (value - min) / (max - min);
     }
 
+    /**
+     * Normaliza una matriz de Character
+     * @param value La matriz a normalzar
+     * @return Matriz de doubles normalizada
+     */
     public static double[][] normalize(char[][] value){
         char[] mins = new char[value.length];
         char[] maxs = new char[value.length];
@@ -28,5 +38,58 @@ public class Normalizers {
         }
 
         return matrix;
+    }
+
+    /**
+     * Normaliza una matriz de Byte
+     * @param value La matriz rectangular a normalizar
+     * @return Matriz de doubles normalizada
+     */
+    public static double[][] normalize(byte[][] value){
+        byte[] mins = new byte[value.length];
+        byte[] maxs = new byte[value.length];
+
+        for(int i = 0; i < value.length; i++){
+            for(int j = 0; j < value[i].length; j++){
+                if(mins[j] > value[i][j]) mins[j] = value[i][j];
+                if(maxs[j] < value[i][j]) maxs[j] = value[i][j];
+            }
+        }
+
+        double[][] matrix = new double[value.length][value[0].length];
+
+        for(int i = 0; i < value.length; i++){
+            for(int j = 0; j < value[i].length; j++){
+                matrix[i][j] = normalizeRange(value[i][j], mins[j], maxs[j]);
+            }
+        }
+
+        return matrix;
+    }
+
+    /**
+     * Normaliza una matriz de Byte tomando normalizando 0x00 a 0 y 0xFF a 1
+     * @param value La matriz rectangular a normalizar
+     * @return Matriz de doubles normalizada
+     */
+    public static double[][] normalizeWithDefaults(byte[][] value){
+        double[][] matrix = new double[value.length][value[0].length];
+
+        for(int i = 0; i < value.length; i++){
+            for(int j = 0; j < value[i].length; j++){
+                matrix[i][j] = normalizeRange(value[i][j], Byte.MIN_VALUE, Byte.MAX_VALUE);
+            }
+        }
+
+        return matrix;
+    }
+
+    public static double[] normalize(byte[] value){
+        double[] data = new double[value.length];
+        for(int j = 0; j < value.length; j++){
+            data[j] = normalizeRange(value[j], Byte.MIN_VALUE, Byte.MAX_VALUE);
+        }
+
+        return data;
     }
 }
--- org/jmagicbrain/utils/Readers.java
+++ org/jmagicbrain/utils/Readers.java
@@ -1,4 +1,29 @@
 package org.jmagicbrain.utils;
 
+import java.io.*;
+
 public class Readers {
+
+    public static byte[] readFileAsBytes(String path) throws IOException {
+        byte[] b;
+        File file = new File(path);
+        b = new byte[(int) file.length()];
+        InputStream is = new FileInputStream(file);
+        is.read(b);
+        is.close();
+
+        return b;
+    }
+
+    public static String reafFileAsString(String path) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new FileReader (path));
+        String line;
+        while ((line = reader.readLine()) != null){
+            sb.append(line);
+        }
+
+        return sb.toString();
+    }
+
 }
